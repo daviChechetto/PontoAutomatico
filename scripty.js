89411227e891607bd3ebf6e20c9642f404072cbe
Davi Chechetto <106183453+daviChechetto@users.noreply.github.com>
2024-08-22 13:12:59 -0300

    Add files via upload

--- scripty.js
+++ scripty.js
@@ -17,21 +17,53 @@ $(document).ready(function() {
         $('form').toggleClass('form-dark-mode')
     })
 
-    /* $('.icone-ajudante').on('mouseenter', function() {
-        $('.modalInformacaoAdicional').show();
-    });
-    $('.icone-ajudante').on('mouseleave', function() {
-        $('.modalInformacaoAdicional').hide(); 
-        console.log("hover");
-    }); */
+
 
     $('.icone-ajudante').hover(function() {
         $('.modalInformacaoAdicional').toggle();
-        console.log("hover");
     });
 
-    /* $('.icone-ajudante').on('hover', function() {
-        $('.modalInformacaoAdicional').toggle();
-        console.log("hover");
-    }); */
+    $('.icone-creditos').click(function() {
+
+        if ($('.creditos').hasClass('esconder-link')){
+            $('.link').toggleClass('esconder-link')
+            $('.link').toggleClass('mostrar-link')
+            $('.creditos').toggle();
+        } else{
+            $('.link').toggleClass('esconder-link')
+            $('.link').toggleClass('mostrar-link')
+            setTimeout(function(){
+                $('.creditos').toggle();
+            }, 240)
+        }
+
+
+    
+        
+
+        $('main').toggleClass('blur')
+        $('header').toggleClass('blur')
+
+    });
+
+
+
+    $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+        if (!$(this).next().hasClass('show')) {
+            $(this).parents('.dropdown-menu').first().find('.show').removeClass('show');
+        }
+        var $subMenu = $(this).next('.dropdown-menu');
+        $subMenu.toggleClass('show');
+
+
+        $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+            $('.dropdown-menu .show').removeClass('show');
+        });
+
+        return false;
+    });
+
+    var date = new Date(), mesAtual = date.getMonth() + 1;
+    $('select').val(mesAtual)
+    new window.VLibras.Widget('https://vlibras.gov.br/app');
 })
